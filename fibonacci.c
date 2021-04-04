e4c2e91e1b120dc58cf836a89f91a488fd00f773
Aleksi Backman <bddubbb@gmail.com>
2021-04-04 07:52:44 +0300

    Start by pointing to 2nd slot of fibonacci list

--- fibonacci.c
+++ fibonacci.c
@@ -9,7 +9,19 @@ int main(void)
 {
 	int i = 0;
 	int fibonacci_numbers[FIBONACCI_LEN] = {0};
+	int* fibonacci_walk = fibonacci_numbers;
+	// TODO fibonacci_numbers[FIBONACCI_LEN - 1] = -1;
 
+	fibonacci_numbers[1] = 1;
+	fibonacci_walk++;
+
+#if DEBUG_ENABLE
+	printf("fibonacci_walk is: %d\n\n", *fibonacci_walk);
+#endif // DEBUG_ENABLE
+
+	// TODO get rid of variable "i" and force stop by inserting -1 to the
+	// end of list. Also should then make the list 1 slot longer as one of
+	// the slots is always used for marking end of list.
 	while(i < FIBONACCI_LEN) {
 		if (fibonacci_numbers[i] < 0) {
 			printf("Encountered negative fibonacci number. Stopping");
