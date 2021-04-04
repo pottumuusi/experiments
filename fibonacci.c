5c1c60526b0a36b176ceb9f72c999ca0f9e4a6f8
Aleksi Backman <bddubbb@gmail.com>
2021-04-04 07:44:44 +0300

    Initialize and print list full of 0s

--- fibonacci.c
+++ fibonacci.c
@@ -1,8 +1,27 @@
 #include<stdio.h>
 
-int main(int argc, char* argv[])
+#define FIBONACCI_NUMBERS_TO_PROCESS 20 // TODO pass this to compiler, instead of coding here
+#define FIBONACCI_LEN FIBONACCI_NUMBERS_TO_PROCESS
+
+#define DEBUG_ENABLE 1
+
+int main(void)
 {
-	printf("argc = %d, argv = %s\n", argc, argv[1]);
+	int i = 0;
+	int fibonacci_numbers[FIBONACCI_LEN] = {0};
+
+	while(i < FIBONACCI_LEN) {
+		if (fibonacci_numbers[i] < 0) {
+			printf("Encountered negative fibonacci number. Stopping");
+			break;
+		}
+
+#if DEBUG_ENABLE
+		printf("%d\n", fibonacci_numbers[i]);
+#endif // DEBUG_ENABLE
+
+		i++;
+	}
 
 	return 0;
 }
